224275b48653767c04bd70903b08f3c53c64109c
Sriram Arjun <sriramarjun2007@gmail.com>
2026-06-24 13:45:30 +0530

    home page update

--- grocery/src/pages/Homepage.jsx
+++ grocery/src/pages/Homepage.jsx
@@ -1,7 +1,9 @@
 import homeimage from "../assets/home.jpg";
-
+import organic from "../assets/organic.png"
+import delivery from "../assets/delivery.png"
 function Home() {
   return (
+    <div>
     <div
       className="h-screen bg-cover bg-center "
       style={{ backgroundImage: `url(${homeimage})` }} >
@@ -17,6 +19,18 @@ function Home() {
         <button className=" text-white text-xl font-bold bg-orange-600 rounded-2xl p-3 w-40 cursor-pointer hover:bg-orange-800">About Us</button>
       </div>
     </div>
+    <div>
+      <p className="text-center pt-12 text-l">WHAT WE OFFER</p>
+      <div className="flex gap-30 pt-10 pl-50">
+      <div className="border-red-600 h-70 w-70 pt-8 pl-10 bg-green-300 border-1 border-red-600">
+        <img src={organic} className="w-50 h-40"/>
+      </div>
+      <div className="border-red-600 h-70 w-70 bg-green-300 border-1 border-red-600">2</div>
+      <div className="border-red-600 h-70 w-70 bg-green-300 border-1 border-red-600">2</div>
+      </div>
+      </div>
+    </div>
+    
   );
 }
 
